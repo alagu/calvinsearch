5dc5015af0d65f8c69b68e1f5996220e5453428e
Alagu <allagappan@gmail.com>
2013-01-22 12:53:55 +0530

    Change to coffee

--- script.js
+++ script.js
@@ -1,44 +1,31 @@
-(function() {
-var word = 'dad';
+(->
+  word = "dad"
+  calvin_search = (word) ->
+    word = word.toLowerCase()
+    result_list = []
+    for comic of calvin
+      console.log "Searching"
+      found = calvin[comic].toLowerCase().indexOf(word)
+      result_list.push comic  unless found is -1
+    result_list
 
-var calvin_search = function(word)
-{
-    word = word.toLowerCase();
-    var result_list = [];
-    for (comic in calvin) {
-        console.log('Searching');
-        found = calvin[comic].toLowerCase().indexOf(word);
-        if (found != -1)
-        {
-            result_list.push(comic);
-        }
-    }
+  renderResults = ->
+    results = $("#results ul li")
     
-    return result_list;
-}
+    #http://query.yahooapis.com/v1/public/yql?q=select%20src%20from%20html%20where%20url%3D%22http%3A%2F%2Fwww.gocomics.com%2Fcalvinandhobbes%2F1993%2F09%2F19%22%20and%20xpath%3D'%2F%2Fp%5B%40class%3D%22feature_item%22%5D%2F%2Fimg'&format=json&diagnostics=true&callback=image_url
+    for resultitem of results
+      console.log $(results[resultitem]).attr("id")
 
-var renderResults = function() {
-    var results = $('#results ul li');
-    for ( resultitem in results){
-        console.log($(results[resultitem]).attr('id'));
-    }
-}
+  $("#search-form").bind "submit", (e) ->
+    $("#status").html "Searching.."
+    console.log $("#search").val()
+    results = (calvin_search($("#search").val()))
+    $("#status").html "Done searching"
+    $("#results").html "<ul></ul>"
+    for id of results
+      $("#results ul").append "<li id=\"" + results[id] + "\">" + calvin[results[id]] + "</li>"
+    $("#status").html "Rendering results"
+    renderResults()
+    false
 
-$('#search-form').bind('submit', function(e) {
-   $('#status').html("Searching..")
-   console.log($('#search').val());
-   var results = (calvin_search($('#search').val()));
-   $('#status').html("Done searching");
-   $('#results').html('<ul></ul>');
-   for(var id in results)
-   {
-       $('#results ul').append('<li id="'+ results[id] +'">' + calvin[results[id]] + '</li>')
-   }
-   
-   $('#status').html("Rendering results");
-   
-   renderResults();
-   return false;
-});
-
-})();
+)()
